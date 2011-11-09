612ef09fe37a6fc79198f20f57eafb7a981ffdbf
Thomas Johnson <NTmatter@gmail.com>
2011-11-10 00:56:06 +0100

    Test data for interactive exploration
    Real test data should be generated as part of the Unit Test classes, however this approach is best suited for rudimentary iterative development and exploration

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,4 +1,7 @@
+import grails.util.Environment
+import filet.FiletUser
 import filet.FiletRole
+import filet.FiletUserFiletRole
 
 class BootStrap {
 
@@ -6,7 +9,23 @@ class BootStrap {
         def senderRole = FiletRole.findByAuthority('ROLE_SENDER') ?: new FiletRole(authority: 'ROLE_SENDER').save(failOnError: true)
         def receiverRole = FiletRole.findByAuthority('ROLE_RECEIVER') ?: new FiletRole(authority: 'ROLE_RECEIVER').save(failOnError: true)
         def adminRole = FiletRole.findByAuthority('ROLE_ADMIN') ?: new FiletRole(authority: 'ROLE_ADMIN').save(failOnError: true)
+        
+        Environment.executeForCurrentEnvironment {
+            development {
+                def test1 = FiletUser.findByUsername('test1@example.com') ?: new FiletUser(username: 'test1@example.com', password: 'pw', enabled: true).save(failOnError: true)
+                if(!test1.authorities.contains(senderRole)) {
+                    FiletUserFiletRole.create(test1, senderRole)
+                }
+                
+                def test2 = FiletUser.findByUsername('test2@not.example.com') ?: new FiletUser(username: 'test2@not.example.com', password: 'pw', enabled: true).save(failOnError: true)
+                
+                if(!test2.authorities.contains(receiverRole)) {
+                    FiletUserFiletRole.create(test2,receiverRole)
+                }
+            }
+        }
     }
+    
     def destroy = {
     }
 }
--- grails-app/domain/filet/Blob.groovy
+++ grails-app/domain/filet/Blob.groovy
@@ -2,6 +2,8 @@ package filet
 
 class Blob {
     String hash
+    boolean uploadComplete = false
+    long size
     def data
     
     static belongsTo = DataStore
