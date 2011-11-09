d046ba8529ab10d3fa875ac0c9c402f31439d23d
Thomas Johnson <NTmatter@gmail.com>
2011-11-09 22:58:20 +0100

    Security: Defining basic roles

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,11 @@
+import filet.FiletRole
+
 class BootStrap {
 
     def init = { servletContext ->
+        def senderRole = FiletRole.findByAuthority('ROLE_SENDER') ?: new FiletRole(authority: 'ROLE_SENDER').save(failOnError: true)
+        def receiverRole = FiletRole.findByAuthority('ROLE_RECEIVER') ?: new FiletRole(authority: 'ROLE_RECEIVER').save(failOnError: true)
+        def adminRole = FiletRole.findByAuthority('ROLE_ADMIN') ?: new FiletRole(authority: 'ROLE_ADMIN').save(failOnError: true)
     }
     def destroy = {
     }
